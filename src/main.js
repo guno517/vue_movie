8afbb4a66c13cbed8ce0c6901f04daa0190757bf
guno517 <guno5170@gmail.com>
2022-12-09 17:38:19 +0900

    plugins 삭제

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue';
 import App from '~/App';
-import fetchPlugin from '~/plugins/fetch';
 import router from './routes/index';
 import store from '~/store';
 
 const app = createApp(App);
-app.use(fetchPlugin, {
-  // 지워도 될듯
-  pluginName: '$fetchMovie',
-});
 
 app.use(router);
 app.use(store);
