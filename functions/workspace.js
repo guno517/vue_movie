ea74cb3795089d709e3c0f36281df506eaa3daba
guno517 <guno5170@gmail.com>
2022-12-09 23:09:54 +0900

    axios get

--- functions/workspace.js
+++ functions/workspace.js
@@ -3,12 +3,9 @@ const { API_KEY } = process.env;
 
 exports.handler = async function (event) {
   const options = JSON.parse(event.body);
-  const { title, method, body } = options;
-  const { data } = await axios({
-    url: `https://www.omdbapi.com?apikey=${API_KEY}&s=${title}&page=3`,
-    method,
-    data: body,
-  });
+  const { title } = options;
+  const url = `https://www.omdbapi.com?apikey=${API_KEY}&s=${title}&page=3`;
+  const { data } = await axios.get(url);
 
   return {
     statusCode: 200,
